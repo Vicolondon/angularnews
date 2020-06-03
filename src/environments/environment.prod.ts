cca0c6015fd057b6569460c9dac202967e77546d
Le Montagner <victorlemonta@gmail.com>
2020-06-03 13:53:10 +0200

    Init project .

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  API_URL: 'https://newsapp.dwsapp.io/api',
+  NEWS_API_TOKEN: 'yourapikey'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  API_URL: 'https://newsapp.dwsapp.io/api',
+  NEWS_API_TOKEN: '70a7e8c389c04a5dbcf2a099b6e14d90'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
